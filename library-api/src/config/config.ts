225c9c89b23e07f29f02c72b586a77afa7ac1ce0
Rashid Ahamed <rashid123321a@gmail.com>
2023-08-02 22:38:23 +0530

    feat: add db connection

--- library-api/src/config/config.ts
+++ library-api/src/config/config.ts
@@ -1,3 +1,11 @@
 export const port = process.env.PORT || 4000;
 
 export const fe_url = 'http://localhost:3000';
+
+export const dbConfig = {
+    host : "localhost",
+    port: "27017",
+    userName: "mongoadmin",
+    password: "secret",
+    dbName: "library-app"
+} 
--- library-api/src/server.ts
+++ library-api/src/server.ts
@@ -1,6 +1,7 @@
 import  express, { Express } from "express";
 import cors from 'cors';
-import { fe_url, port } from "./config/config";
+import mongoose from 'mongoose';
+import { dbConfig, fe_url, port } from "./config/config";
 import { getLogger } from "./helpers/logger";
 
 const logger = getLogger('SERVER');
@@ -14,6 +15,10 @@ app.use(cors({
     origin: fe_url
   }));
 
-app.listen(port, () => {
-    logger.info(`Server is listning ar: ${port}`);
-});
+mongoose.connect(`mongodb://${dbConfig.userName}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}?authSource=admin`)
+.then(result => 
+    app.listen(port, () => {
+        logger.info(`Server is listning ar: ${port}`);
+    })
+    )
+.catch(err => logger.error(err));
