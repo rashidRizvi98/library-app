f04a687040e26645b6c07a8c492f8c43c1e09434
Rashid Ahamed <rashid123321a@gmail.com>
2023-08-04 22:10:38 +0530

    fix: properly catch error on book create, update

--- library-app-fe/src/services/book.ts
+++ library-app-fe/src/services/book.ts
@@ -2,14 +2,27 @@ import { baseApiUrl } from "../config/config";
 import { IBook, IBookCreateResponse, IBookResponse } from "../models/book";
 
 
-export const createBook = (payload: Partial<IBook>) => {
-    return fetch(`${baseApiUrl}/books`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) }).then(data => data.json()).then(data => data as IBookCreateResponse);
+export const createBook = async(payload: Partial<IBook>) => {
+    const response = await fetch(`${baseApiUrl}/books`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
+    const data = await response.json();
+
+    if (response.status != 201) {
+        return Promise.reject(response);
+    }
+    return data as IBookCreateResponse;
 } 
 
 export const getBook = (id: string) => {
     return fetch(`${baseApiUrl}/books/${id}`, { method: 'GET', headers: { 'Content-Type': 'application/json' } }).then(data => data.json()).then(data => data as IBookResponse);
 }
 
-export const updateBook = (payload: Partial<IBook>) => {
-    return fetch(`${baseApiUrl}/books`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) }).then(data => data.json()).then(data => data as IBookCreateResponse);
+export const updateBook = async(payload: Partial<IBook>) => {
+    const response = await fetch(`${baseApiUrl}/books`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
+    const data = await response.json();
+
+    if (response.status != 200) {
+        return Promise.reject(response);
+    }
+
+    return data as IBookCreateResponse;
 } 
